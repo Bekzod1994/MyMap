3a2a22041cc8fd21defd19eab59b0e3c86ce35d9
Bekzod <bekzodqalanadarov1994@gmail.com>
2025-02-27 10:25:01 +0500

    remove unused

--- app/src/main/java/com/map/address/utils/Constants.kt
+++ app/src/main/java/com/map/address/utils/Constants.kt
@@ -5,9 +5,6 @@ import com.yandex.mapkit.map.CameraPosition
 
 class Constants {
     companion object {
-        const val BASE_URL =
-            "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
-
         val tashkentCameraPosition = CameraPosition(Point(41.232322, 69.24342323), 12f, 1.0f, 0.0f)
     }
 }
